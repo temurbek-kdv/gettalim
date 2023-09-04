6fb6bbf1f56d9dc3c586ed9256219f3d10b73112
temurbek-kdv <temur.kdv@gmail.com>
2023-09-04 16:09:34 +0500

    base repository added

---
+++ src/GetTalim.DataAccess/Repositories/BaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using Npgsql;
+
+namespace GetTalim.DataAccess.Repositories;
+
+public class BaseRepository
+{
+    protected readonly NpgsqlConnection _connection;
+    public BaseRepository()
+    {
+        Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+        ////_connection = new NpgsqlConnection("Host=localhost; Port=5432; Database=gettalim-db; User Id=postgres; Password=ll;");
+        
+        _connection = new NpgsqlConnection("Host=db-postgresql-lon1-56814-do-user-14588545-0.b.db.ondigitalocean.com; Port=25060; Database=gettalim-db; User Id=doadmin; Password=AVNS_9n5XkthWFqLIltZLhLQ;");
+    }
+}
