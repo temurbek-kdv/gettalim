6bac74f73693096ffa4f261ee8b7adc9cc2b140a
Temurbek <96979969+temurkodirov@users.noreply.github.com>
2023-10-28 17:11:05 +0500

    docker database

--- src/GetTalim.DataAccess/Repositories/BaseRepository.cs
+++ src/GetTalim.DataAccess/Repositories/BaseRepository.cs
@@ -8,8 +8,9 @@ public class BaseRepository
     public BaseRepository()
     {
         Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-        ////_connection = new NpgsqlConnection("Host=localhost; Port=5432; Database=gettalim-db; User Id=postgres; Password=ll;");
-        
-        _connection = new NpgsqlConnection("Host=db-postgresql-lon1-56814-do-user-14588545-0.b.db.ondigitalocean.com; Port=25060; Database=gettalim-db; User Id=doadmin; Password=AVNS_9n5XkthWFqLIltZLhLQ;");
+        //_connection = new NpgsqlConnection("Host=localhost; Port=5432; Database=gettalim-db; User Id=postgres; Password=ll;");
+        _connection = new NpgsqlConnection("Host=gettalim-database-host; Port=5432; Database=gettalim-db; User Id=postgres_admin; Password=QQ!!qq11;");
+
+        ////_connection = new NpgsqlConnection("Host=db-postgresql-lon1-56814-do-user-14588545-0.b.db.ondigitalocean.com; Port=25060; Database=gettalim-db; User Id=doadmin; Password=AVNS_9n5XkthWFqLIltZLhLQ;");
     }
 }
