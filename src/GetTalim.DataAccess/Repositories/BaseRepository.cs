b50dd0675666d4b9de4d00ea5f4b4f8b21e93faf
Temurbek <96979969+temurkodirov@users.noreply.github.com>
2023-10-28 18:23:30 +0500

    new database

--- src/GetTalim.DataAccess/Repositories/BaseRepository.cs
+++ src/GetTalim.DataAccess/Repositories/BaseRepository.cs
@@ -9,8 +9,8 @@ public class BaseRepository
     {
         Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
         //_connection = new NpgsqlConnection("Host=localhost; Port=5432; Database=gettalim-db; User Id=postgres; Password=ll;");
-        _connection = new NpgsqlConnection("Host=gettalim-database-host; Port=5432; Database=gettalim-db; User Id=postgres_admin; Password=QQ!!qq11;");
+        //_connection = new NpgsqlConnection("Host=gettalim-database-host; Port=5432; Database=gettalim-db; User Id=postgres_admin; Password=QQ!!qq11;");
 
-        ////_connection = new NpgsqlConnection("Host=db-postgresql-lon1-56814-do-user-14588545-0.b.db.ondigitalocean.com; Port=25060; Database=gettalim-db; User Id=doadmin; Password=AVNS_9n5XkthWFqLIltZLhLQ;");
+        _connection = new NpgsqlConnection("Host=db-postgresql-nyc3-62486-do-user-14993247-0.c.db.ondigitalocean.com; Port=25060; Database=gettalim-db; User Id=doadmin; Password=AVNS_6X-S9VpP5KFVSzOvFXt;");
     }
 }
